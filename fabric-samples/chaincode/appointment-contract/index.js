c671e5f6bd7c99da81d0a871116542b3b8490d9a
Devesh Tiwari <161412073+deveshyaara@users.noreply.github.com>
2025-12-11 14:14:09 +0000

    Updated file: start.sh

--- fabric-samples/chaincode/appointment-contract/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const AppointmentContract = require('./lib/appointment-contract');
-
-module.exports.AppointmentContract = AppointmentContract;
-module.exports.contracts = [AppointmentContract];
